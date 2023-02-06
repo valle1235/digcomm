e0658c1c27d256bdd2a3e4a0e59cf974a62c1392
valle1235 <vallen0002@gmail.com>
2023-02-06 13:39:13 +0100

    Update sync.m

--- sync.m
+++ sync.m
@@ -18,9 +18,18 @@ function t_samp = sync(mf, b_train, Q, t_start, t_end)
 %
 % Output:
 %   t_samp = sampling instant for first symbol
-    b_train_qpsk = qpsk(b_train);
-    corr = xcorr(b_train_qpsk,mf);
-    corr = abs(corr(t_start:t_end));
-    [~,alignment] = max(corr); %Find the correct alignment of mf
-    t_samp = (alignment+t_start-1)/Q;
+    c = qpsk(b_train);
+    L = length(c);
+    possible_t_samp = [];
+    for t = t_start:1:t_end
+        a = 0;
+        for k = 1:1:L
+            a=a+mf(k*Q+t)*c(k);
+        end
+        possible_t_samp = [possible_t_samp a];
+    end
+    %corr = xcorr(mf(t_start:t_end),b_train_qpsk_replica);
+    %stem(abs(possible_t_samp))
+    [~,t_samp] = max(abs(possible_t_samp)); %Find the correct alignment of mf
+    t_samp = t_samp + t_start -1;
 end
